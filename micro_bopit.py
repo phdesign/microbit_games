b3ba733f90c3a1edea2ddd1a5ecfa86bfb43c06a
Paul Heasley <paul@phdesign.com.au>
2021-08-11 20:25:40 +1000

    feat[bopit]: Add pin logo as an option

--- micro_bopit.py
+++ micro_bopit.py
@@ -7,6 +7,7 @@ import music
 class Input:
     BUTTON_A = 1
     BUTTON_B = 2
+    PIN_LOGO = 3
 
 class Option:
     def __init__(self, prompt, exected, sound):
@@ -16,7 +17,8 @@ class Option:
 
 OPTIONS = [
     Option(Image.ARROW_W, Input.BUTTON_A, "D4:4"),
-    Option(Image.ARROW_E, Input.BUTTON_B, "E4:4")
+    Option(Image.ARROW_E, Input.BUTTON_B, "E4:4"),
+    Option(Image.ARROW_N, Input.PIN_LOGO, "F4:4")
 ]
 WAIT_START_MS = 1500
 DECAY_RATE = 50
@@ -35,6 +37,8 @@ def wait_for_input(wait_for):
             return Input.BUTTON_A
         elif button_b.is_pressed():
             return Input.BUTTON_B
+        elif pin_logo.is_touched():
+            return Input.PIN_LOGO
 
         elapsed = running_time() - start
         if elapsed > wait_for:
@@ -44,8 +48,7 @@ def play():
     score = 0
 
     display.clear()
-    music.play(music.PRELUDE, wait=False)
-    sleep(1)
+    music.play(music.JUMP_UP, wait=False)
     display.show("3")
     sleep(0.7)
     display.show("2")
@@ -59,7 +62,7 @@ def play():
         elapsed_sec = (running_time() - start) / 1000
         wait_ms = round(wait_decay(elapsed_sec), 4)
 
-        option = OPTIONS[randint(0, 1)]
+        option = OPTIONS[randint(0, 2)]
         display.show(option.prompt)
         music.play(option.sound)
 
@@ -69,6 +72,7 @@ def play():
             display.clear()
             sleep(round(wait_ms / 2000, 4))
         else:
+            music.play(music.POWER_DOWN, wait=False)
             display.show(Image.NO)
             sleep(1)
             break
@@ -83,7 +87,7 @@ if __name__ == "__main__":
             score = play()
             if score > high_score:
                 high_score = score
-                music.play(music.POWER_UP, wait=False)
+                music.play(music.PRELUDE, wait=False)
                 display.show(Image.HAPPY)
                 sleep(1)
                 display.scroll("High score: {:d}".format(score), wait=False)
